7eb6866c77fa9e889a782a2e8a51e08c8140e83f
Chris Kennedy <chrisken@gmail.com>
2019-12-25 22:24:39 -0800

    begin regression implementation

--- R/LearnerRegrDbarts.R
+++ R/LearnerRegrDbarts.R
@@ -1,10 +1,10 @@
-#' @title Classification Dbarts Learner
+#' @title Regression Dbarts Learner
 #'
-#' @aliases mlr_learners_classif.dbarts
-#' @format [R6::R6Class] inheriting from [mlr3::LearnerClassif].
+#' @aliases mlr_learners_regr.dbarts
+#' @format [R6::R6Class] inheriting from [mlr3::LearnerRegr].
 #'
 #' @description
-#' A [mlr3::LearnerClassif] for a classification dbarts implemented in dbarts::dbarts()] in package \CRANpkg{dbarts}.
+#' A [mlr3::LearnerRegr] for a classification dbarts implemented in dbarts::dbarts()] in package \CRANpkg{dbarts}.
 #'
 # TODO: add references.
 # @references
@@ -14,24 +14,24 @@
 # \url{https://doi.org/10.1023/A:1010933404324}
 #'
 #' @export
-LearnerClassifDbarts = R6Class("LearnerClassifDbarts", inherit = LearnerClassif, # Adapt the name to your learner. For regression learners inherit = LearnerRegr.
+LearnerRegrDbarts = R6Class("LearnerRegrDbarts", inherit = LearnerRegr, # Adapt the name to your learner. For regression learners inherit = LearnerRegr.
   public = list(
     initialize = function() {
       ps = ParamSet$new( # parameter set using the paradox package
         params = list(
           # These reflect the defaults used by the dbarts package.
           ParamInt$new(id = "ntree", default = 200L, lower = 1L, tags = "train"),
-          # Only used for continuous models, so can remove from LearnerClassif.
-          #ParamDbl$new(id = "sigest", default = NULL, lower = 0, tags = "train"),
-          # Only used for continuous models, so can remove from LearnerClassif.
-          #ParamInt$new(id = "sigdf", default = 3L, lower = 1L, tags = "train"),
-          # Only used for continuous models, so can remove from LearnerClassif.
-          #ParamDbl$new(id = "sigquant", default = 0.90, lower = 0, upper = 1, tags = "train"),
+          # Only used for continuous models
+          ParamDbl$new(id = "sigest", default = NULL, lower = 0, tags = "train"),
+          # Only used for continuous models
+          ParamInt$new(id = "sigdf", default = 3L, lower = 1L, tags = "train"),
+          # Only used for continuous models
+          ParamDbl$new(id = "sigquant", default = 0.90, lower = 0, upper = 1, tags = "train"),
           ParamDbl$new(id = "k", default = 2.0, lower = 0, tags = "train"),
           ParamDbl$new(id = "power", default = 2.0, lower = 0, tags = "train"),
           ParamDbl$new(id = "base", default = 0.95, lower = 0, tags = "train"),
           # Not applicable for LearnerRegr
-          ParamDbl$new(id = "binaryOffset", default = 0.0, tags = "train"),
+          #ParamDbl$new(id = "binaryOffset", default = 0.0, tags = "train"),
           ParamInt$new(id = "ndpost", default = 1000L, lower = 1L, tags = "train"),
           ParamInt$new(id = "nskip", default = 100L, lower = 0L, tags = "train"),
           ParamInt$new(id = "printevery", default = 100L, lower = 0L, tags = "train"),
@@ -53,14 +53,15 @@ LearnerClassifDbarts = R6Class("LearnerClassifDbarts", inherit = LearnerClassif,
 
       super$initialize(
         # see the mlr3book for a description: https://mlr3book.mlr-org.com/extending-mlr3.html
-        id = "classif.dbarts",
+        id = "regr.dbarts",
         packages = "dbarts",
         feature_types = c("integer", "numeric", "factor", "ordered"),
-        predict_types = c("response", "prob"),
+        # TODO: add "se" to the list of predict types.
+        predict_types = c("response"),
         param_set = ps,
         # TODO: add importance.
         # Parallel is giving an autotest error.
-        properties = c("weights", "twoclass")#, "parallel")
+        properties = c("weights")#, "parallel")
       )
     },
 
@@ -100,40 +101,15 @@ LearnerClassifDbarts = R6Class("LearnerClassifDbarts", inherit = LearnerClassif,
       # Other possible vars: offset.test, combineChains, ...
       setDF(newdata)
 
-
-      # TODO: Can't get prediction to pass the sanity checks on factor level ordering.
-      # Via https://github.com/mlr-org/mlr3learners/blob/master/R/LearnerClassifXgboost.R#L171
-      lvls = rev(task$class_names)
-      #lvls = task$class_names
-      # Via https://github.com/mlr-org/mlr3learners/blob/094ac91b690c262d1e911cbbef98ae69fc9d2386/R/LearnerClassifLogReg.R#L48
-      #lvls = levels(self$model$data[[task$target_names]])
-      nlvl = length(lvls)
-
       # This will return a matrix of predictions, where each column is an observation
       # and each row is a sample from the posterior.
       p = invoke(predict, self$model, test = newdata, .args = pars)
 
       # Transform predictions.
       # TODO: confirm that this is the correct element name.
-      pred = colMeans(stats::pnorm(p))
+      pred = colMeans(p)
 
-      # Via https://github.com/mlr-org/mlr3learners/blob/master/R/LearnerClassifXgboost.R#L171
-      # TODO: need to do a PR to get this function exported.
-      prob = mlr3learners:::prob_vector_to_matrix(pred, lvls)
-
-      # Return a prediction object with PredictionClassif$new() or PredictionRegr$new()
-      if (self$predict_type == "response") {
-        # Via https://github.com/mlr-org/mlr3learners/blob/master/R/LearnerClassifXgboost.R#L171
-#        i = max.col(prob, ties.method = "random")
-#        response = factor(colnames(prob)[i], levels = lvls)
-        #PredictionClassif$new(task = task, response = response)
-        response = ifelse(pred < 0.5, lvls[1L], lvls[2L])
-
-        browser()
-        PredictionClassif$new(task = task, response = response)
-      } else {
-        PredictionClassif$new(task = task, prob = prob)
-      }
+      PredictionRegr$new(task = task, response = response)
     }
 
     # Add method for importance, if learner supports that.
