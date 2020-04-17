78a974b94111cb67bf79bf65818187a38b7ab391
pat-s <patrick.schratz@gmail.com>
2020-04-17 09:03:38 +0200

    upd pkg infra

--- R/tic.R
+++
@@ -1,4 +0,0 @@
-# installs dependencies, runs R CMD check, runs covr::codecov()
-do_package_checks()
-
-do_drat("mlr3learners/mlr3learners.drat")
--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,4 @@
 library(testthat)
 library(mlr3learners.dbarts)
 
-test_check("mlr3learners.dbarts")
+# test_check("mlr3learners.dbarts")
